f568cc278cee323af5f47c055e4648866d7a999d
sahilahmed21 <159174363+sahilahmed21@users.noreply.github.com>
2025-06-12 01:22:25 +0530

    orientation ass

---
+++ orientation/ass3.js
@@ -0,0 +1,7 @@
+const crypto = require('crypto');
+function findhashwithprefix(prefix) {
+    let input = 0;
+    while (true) {
+        let inputStr = 'harkirat => Raman | Rs 100 </br> Ram => Ankit | Rs 10' + input.toString();
+    }
+}
