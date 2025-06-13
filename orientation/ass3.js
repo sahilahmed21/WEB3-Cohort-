8862cea17946485c5b834f563c169443326b2910
sahilahmed21 <159174363+sahilahmed21@users.noreply.github.com>
2025-06-14 00:13:43 +0530

    binary conversions

--- orientation/ass3.js
+++ orientation/ass3.js
@@ -3,5 +3,11 @@ function findhashwithprefix(prefix) {
     let input = 0;
     while (true) {
         let inputStr = 'harkirat => Raman | Rs 100 </br> Ram => Ankit | Rs 10' + input.toString();
+        let hash = crypto.createHash('sha256').update(inputStr).digest('hex');
+        if (hash.startsWith(prefix)) {
+            return { input: inputStr, hash: hash };
+        }
+        input++;  // tries next nonce
+
     }
 }
---
+++ publicKeyCryptography/codes.js
@@ -0,0 +1,49 @@
+// function bytesToAscii(byteArray) {
+//     return byteArray.map(byte => String.fromCharCode(byte)).join('');
+// }
+
+// // Example usage:
+// const bytes = [72, 101, 108, 108, 111]; // Corresponds to "Hello"
+// const asciiString = bytesToAscii(bytes);
+// console.log(asciiString); // Output: "Hello"
+
+
+
+
+// function asciiToBytes(asciiString) {
+//     const byteArray = [];
+//     for (let i = 0; i < asciiString.length; i++) {
+//         byteArray.push(asciiString.charCodeAt(i));
+//     }
+//     return byteArray;
+// }
+
+// // Example usage:
+// const ascii = "Hello";
+// const byteArray = asciiToBytes(ascii);
+// console.log(byteArray); // Output: [72, 101, 108, 108, 111]
+
+
+
+
+// function bytesToAscii(byteArray) {
+//     return new TextDecoder().decode(byteArray);
+// }
+
+// // Example usage:
+// const bytes = new Uint8Array([72, 101, 108, 108, 111]); // Corresponds to "Hello"
+// const asciiString = bytesToAscii(bytes);
+// console.log(asciiString); // Output: "Hello"
+
+
+
+
+
+// function asciiToBytes(asciiString) {
+//     return new Uint8Array([...asciiString].map(char => char.charCodeAt(0)));
+// }
+
+// // Example usage:
+// const ascii = "Hello";
+// const byteArray = asciiToBytes(ascii);
+// console.log(byteArray); // Output: Uint8Array(5) [72, 101, 108, 108, 111]
